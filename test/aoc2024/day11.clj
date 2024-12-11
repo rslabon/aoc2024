71d20b2e849a9569df34f7cd8f5f95fc8f2d1aff
rslabon <rslabon@gmail.com>
2024-12-11 14:40:30 +0100

    day 11 - part 2

--- test/aoc2024/day11.clj
+++ test/aoc2024/day11.clj
@@ -5,36 +5,32 @@
 (def apply-rule
   (memoize (fn [number blink]
              (if (= blink 0)
-               number
+               1
                (let [number-str (str number)
                      length (count number-str)
-                     half-length (int (Math/floor (/ length 2)))
-                     ;_ (println number blink)
-                     ]
+                     half-length (int (Math/floor (/ length 2)))]
                  (cond
                    (= number 0) (apply-rule 1 (dec blink))
-                   (even? (count number-str)) [(apply-rule (bigint (apply str (take half-length number-str))) (dec blink))
-                                               (apply-rule (bigint (apply str (take half-length (drop half-length number-str)))) (dec blink))]
+                   (even? (count number-str)) (+ (apply-rule (bigint (apply str (take half-length number-str))) (dec blink))
+                                                 (apply-rule (bigint (apply str (take half-length (drop half-length number-str)))) (dec blink)))
                    :else (apply-rule (* number 2024) (dec blink))
                    ))
                )
              )))
 
-(defn part1 [line blinks]
+(defn count-stones [line blinks]
   (let [stones (str/split line #" ")
         stones (mapv bigint stones)
-        stones (mapv #(apply-rule % blinks) stones)
-        stones (flatten stones)
-        ;_ (println stones)
-        ]
-    (count stones)
+        stones (mapv #(apply-rule % blinks) stones)]
+    (apply + stones)
     )
   )
 
 (deftest a-test
-  (testing "part1"
-    (is (= (part1 "125 17" 6) 22))
-    (is (= (part1 "125 17" 25) 55312))
-    (is (= (part1 "6563348 67 395 0 6 4425 89567 739318" 25) 184927))
+  (testing "count-stones"
+    (is (= (count-stones "125 17" 6) 22))
+    (is (= (count-stones "125 17" 25) 55312))
+    (is (= (count-stones "6563348 67 395 0 6 4425 89567 739318" 25) 184927))
+    (is (= (count-stones "6563348 67 395 0 6 4425 89567 739318" 75) 220357186726677))
     )
   )
